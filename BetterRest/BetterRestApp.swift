f461f2fdc1707b00248a7a44038b8317216c2167
Anatoly <aapolyudov@icloud.com>
2020-09-25 21:44:15 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Анатолий on 25.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
